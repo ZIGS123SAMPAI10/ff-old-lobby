fd66ee20499116dc1b8513002efea7be6ea42308
ZIGS123SAMPAI10 <qowialkhoir@gmail.com>
2026-07-07 08:18:30 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,9 +4,9 @@ import socket
 app = Flask(__name__)
 
 # ====================================================================
-# TARGET OPERAN: LANGSUNG KE JEMBATAN SERVEO TERMUX (PAS PORT 14000)
+# LINK DAN PORT ASLI LU, KAGAK GUA SENTUH ATAU UBAH STRUKTURNYA BRE! 🗿
 # ====================================================================
-PAGEKITE_DOMAIN = "serveo.net"
+PAGEKITE_DOMAIN = "ffoldprivatserver.serveo.net"
 PAGEKITE_PORT   = 14000
 # ====================================================================
 
@@ -14,8 +14,8 @@ PAGEKITE_PORT   = 14000
 @app.route("/<path:path>", methods=["GET", "POST"])
 def catch_all(path):
     try:
-        # 1. Ambil IP Angka dari Serveo secara real-time
-        ip_angka = socket.gethostbyname(PAGEKITE_DOMAIN)
+        # 1. Ambil IP Angka dari Serveo secara real-time lewat domain utamanya
+        ip_angka = socket.gethostbyname("serveo.net")
         
         # 2. Buka pipa socket TCP langsung ke Termux via Serveo
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,11 +23,11 @@ def catch_all(path):
         s.connect((ip_angka, PAGEKITE_PORT))
         
         # 3. Ambil data mentah dari game (GET ver.php atau POST biner login, oper semua!)
-        # Kita rakit ulang request HTTP/Biner aslinya agar dibaca utuh oleh Termux
+        # Host header otomatis disamain pake variabel PAGEKITE_DOMAIN di atas
         raw_headers = f"{request.method} /{path} HTTP/1.1\r\nHost: {PAGEKITE_DOMAIN}\r\n\r\n".encode()
         raw_body = request.get_data()
         
-        # Tembakkan langsung paketnya menuju Termux
+        # Tembakkan langsung paketnya menuju Termux lu
         s.sendall(raw_headers + raw_body)
         
         # 4. Ambil jawaban dari Termux, lalu oper balik ke game FF lu
